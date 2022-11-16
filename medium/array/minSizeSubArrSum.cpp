1d587b642d81acc91be73d57221ddd59aa5a957b
Ripunjay choudhury <ripunjaychoudhury42@gmail.com>
2022-11-16 18:39:38 +0530

    Add files via upload

---
+++ medium/array/minSizeSubArrSum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int minSubArrayLen(int target, vector<int> &nums)
+{
+    int left = 0;
+    int sum = 0;
+    int minLength = INT_MAX;
+    for (int right = 0; right < nums.size(); ++right)
+    {
+        sum += nums[right];
+        while (sum >= target)
+        {
+            minLength = min(minLength, right - left + 1);
+            sum -= nums[left];
+            ++left;
+        }
+    }
+    return (minLength != INT_MAX) ? minLength : 0;
+}
+};
