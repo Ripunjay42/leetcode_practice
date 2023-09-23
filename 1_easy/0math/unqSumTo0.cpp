b0c8d13697a5c836a461c249ca40656d1523fd51
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2023-09-23 23:18:03 +0530

    math23

---
+++ 1_easy/0math/unqSumTo0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) 
+    {
+        vector <int> ans;
+        for (int i=0; i<n; i++)
+        {
+            ans.push_back(i*2-n+1);
+        }
+        return ans;
+    }
+};
