d16643b667b9edc9b52270a4d9a21d595792ea5a
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2023-10-02 20:16:44 +0530

    2 oct

---
+++ 3_hard/2string/distinctStr.cpp
@@ -0,0 +1,23 @@
+class Solution{
+  public:	
+	int distinctSubsequences(string s)
+	{
+	     int mod=1e9+7;
+        int n=s.size();
+        unordered_map<char,int>m;
+        vector<int>dp(n+1);
+        dp[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]= (2*dp[i-1])%mod;
+            char ch=s[i-1];
+            if(m[ch])
+            {
+                int j=m[ch];
+                dp[i]=(dp[i]-dp[j-1]+mod)%mod;
+            }
+            m[ch]=i;
+        }
+        return dp[n];
+	}
+};
