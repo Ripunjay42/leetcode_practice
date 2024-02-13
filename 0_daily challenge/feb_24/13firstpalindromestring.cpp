d92b60345c72f60bf2cafa90710046d59a66f0b8
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-02-13 17:59:34 +0530

    13 feb

---
+++ 0_daily challenge/feb_24/13firstpalindromestring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) 
+    {
+        for(auto i:words)
+        {
+            string s=i;  
+            reverse(s.begin(),s.end());  
+            if(s==i)
+            {
+                return s;
+            }
+        }
+        return "";
+    }
+};
