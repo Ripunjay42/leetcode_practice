d414e97679277dfc0725be8c6e5a3171e5790b0c
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-02-17 00:15:57 +0530

    16 feb

---
+++ 0_daily challenge/feb_24/16unqIntAfterKrmv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) 
+    {
+        unordered_map<int, int> mp;
+        for(auto i:arr)
+        {
+            mp[i]++;
+        }
+        int n=mp.size();
+        vector<pair<int, int>> temp(n);
+        int j=0;
+        for(auto m:mp)
+        {
+            temp[j].first = m.second;
+            temp[j].second = m.first;
+            j++;
+        }
+        sort(temp.begin(), temp.end());
+        j=0;
+        int ans=temp.size();
+        while(k>0)
+        {
+             while (temp[j].first > 0 && k > 0) 
+            {
+                temp[j].first--;
+                k--;
+            }
+            if (temp[j].first == 0) {
+                j++;
+                ans--;
+            }
+        }
+        return ans;
+    }
+};
