71e57bf934592d4bad1cc51e21b00822287f177d
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-08-15 04:25:38 +0530

    feat: Implement binary search algorithm to search in a rotated sorted array

---
+++ 1_easy/6binary_search/srchInRotatedeSortedArr.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool search(vector<int>& arr, int k) 
+    {
+          int n = arr.size(); // size of the array.
+         int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return true;
+
+        //Edge case:
+        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+    }
+};
