4792383875e97ac287791bb9ba4f50126c0cc9d9
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-09-07 22:09:06 +0530

    feat: Add solution to split a linked list into odd and even parts

---
+++ 2_medium/5linkedList/oddEvenLL.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) 
+    {
+        
+        if(!head || !head->next || !head->next->next)
+            return head;
+        ListNode *odd=head, *oddhead=odd;
+        ListNode *even=head->next, *evenhead=even;
+        while(even && even->next)
+        {
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=evenhead;
+        return oddhead;
+    }
+};
