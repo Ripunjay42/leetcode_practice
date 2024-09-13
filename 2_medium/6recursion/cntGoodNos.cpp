8921e57623cbb22cae9851e6364989c4378997d7
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-09-13 22:21:44 +0530

    feat: Refactor countGoodNumbers and power functions for clarity and efficiency

--- 2_medium/6recursion/cntGoodNos.cpp
+++ 2_medium/6recursion/cntGoodNos.cpp
@@ -1,23 +1,26 @@
 #define mod 1000000007
 class Solution {
-private:
+public:
+    int countGoodNumbers(long long n) 
+    {
+        long long oddPlaces = n/2;
+        long long evenPlaces = n/2 + n%2;
+        return (power(5, evenPlaces) * power(4, oddPlaces))%mod;
+    }
+
     long long power(long long x, long long n){
-        if(n == 0){
+        if(n == 0)
+        {
             return 1;
         }
-        long long ans = power(x, n/2);
-        ans *= ans;
-        ans %= mod;
+        long long y = power(x, n/2);
+        y =y*y;
+        y =y  % mod;
         if(n%2==1){
-            ans *= x;
-            ans %= mod;
+            y =y* x;
+            y=y% mod;
         }
-        return ans;
-    }
-public:
-    int countGoodNumbers(long long n) {
-        long long numberOfOddPlaces = n/2;
-        long long numberOfEvenPlaces = n/2 + n%2;
-        return (power(5, numberOfEvenPlaces) * power(4, numberOfOddPlaces))%mod;
-    }
+        return y;
+    }  
+
 };
