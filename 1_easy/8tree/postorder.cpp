c385d20e13475ed9fc6e0d738ba5fcff6722b4ef
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-09-28 06:02:18 +0530

    sad

---
+++ 1_easy/8tree/postorder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        postorder(root, res);
+        return res;
+    }
+
+private:
+    void postorder(TreeNode* root, vector<int>& res) {
+        if (!root) return;
+        postorder(root->left, res);
+        postorder(root->right, res);
+        res.push_back(root->val);
+    }
+};
