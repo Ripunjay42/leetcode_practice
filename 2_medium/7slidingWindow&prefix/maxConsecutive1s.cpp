a459d2c89fb68fae6f34039a7b445e3d11ada79b
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-10-06 02:03:26 +0530

    6 oct

---
+++ 2_medium/7slidingWindow&prefix/maxConsecutive1s.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) 
+    {
+        int ans=0;
+        int l=0;
+        for(int r=0;r<nums.size();r++)
+        {
+            if(nums[r]==0)
+                k--;
+            
+            while(k<0)
+            {
+                if(nums[l]==0)
+                {
+                    k++;
+                }
+                l++;
+            }
+
+            ans=max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
