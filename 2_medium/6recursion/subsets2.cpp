29436b330bfe43be6504dda67ef23d26ae9f421d
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2025-01-28 17:36:06 +0530

    feat: Refactor subsetsWithDup function for clarity and consistency

--- 2_medium/6recursion/subsets2.cpp
+++ 2_medium/6recursion/subsets2.cpp
@@ -1,20 +1,23 @@
 class Solution {
-   private:
-      void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+         vector<vector<int>> ans;
+         vector<int> ds;
+         sort(nums.begin(), nums.end());
+         fnd(0,  nums, ds, ans);
+         return ans;
+    }
+
+    void fnd(int ind, vector<int> &nums, vector<int>& ds, vector<vector<int>>& ans)
+    {
          ans.push_back(ds);
-         for (int i = ind; i < nums.size(); i++) {
+         for (int i = ind; i < nums.size(); i++) 
+         {
             if (i != ind && nums[i] == nums[i - 1]) continue;
             ds.push_back(nums[i]);
-            findSubsets(i + 1, nums, ds, ans);
+            fnd(i + 1, nums, ds, ans);
             ds.pop_back();
          }
-      }
-   public:
-      vector < vector < int >> subsetsWithDup(vector < int > & nums) {
-         vector < vector < int >> ans;
-         vector < int > ds;
-         sort(nums.begin(), nums.end());
-         findSubsets(0, nums, ds, ans);
-         return ans;
-      }
+    }
 };
