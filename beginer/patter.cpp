1e9b648f149170eb01b4bde9649314c57629d397
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2025-04-17 18:10:00 +0530

    feat: Add pattern printing program to generate pyramid structure

---
+++ beginer/patter.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        //space
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<i*2+1;j++)
+        {
+            cout<<"*";
+        }
+         //space
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
